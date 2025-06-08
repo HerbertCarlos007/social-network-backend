fc479166749a437f4099645a24960e8b3ef3e2b6
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2025-06-08 00:45:49 -0300

    feat: criando pattern repository

---
+++ app/Repositories/Contracts/PostRepositoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+use App\DTO\PostDTO;
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Collection;
+
+
+interface PostRepositoryInterface
+{
+    public function create(PostDTO $dto): Post;
+    public function index(): Collection;
+    public function show(Post $post): Post;
+    public function update(PostDTO $dto, Post $post ): Post;
+    public function destroy(Post $post): bool;
+}
---
+++ app/Repositories/PostRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repositories;
+
+use App\DTO\PostDTO;
+use App\Models\Post;
+use App\Repositories\Contracts\PostRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
+
+class PostRepository implements PostRepositoryInterface
+{
+    public function create(PostDTO $dto): Post
+    {
+        return Post::create($dto->toArray());
+    }
+
+    public function index(): Collection
+    {
+        return Post::with('user')->get();
+    }
+
+    public function show(Post $post): Post
+    {
+        return $post;
+    }
+
+    public function update(PostDTO $dto, Post $post ): Post
+    {
+        $post->update($dto->toArray());
+        return $post;
+    }
+
+    public function destroy(Post $post): bool
+    {
+        return $post->delete();
+    }
+
+}
