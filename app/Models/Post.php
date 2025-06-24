77115cdfdbf9d195893b541571ed6c93330454e0
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2025-06-24 00:15:53 -0300

    feat: adicionando tipagem no relacionamento

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
@@ -21,12 +22,12 @@ class Post extends Model
 
     }
 
-    public function likes()
+    public function likes(): HasMany
     {
         return $this->hasMany(Like::class);
     }
 
-    public function comments()
+    public function comments(): HasMany
     {
         return $this->hasMany(Comment::class);
     }
