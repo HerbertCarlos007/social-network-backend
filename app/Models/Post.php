74661e84447636596a1541962458ff507c9ce906
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2025-06-24 00:18:59 -0300

    feat: adicionando tipagem no relacionamento

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
@@ -16,7 +17,7 @@ class Post extends Model
         'image_post_url',
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
 
