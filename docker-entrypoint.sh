33d5c8140f1d88ca7a5dff2c8802dd8e6affbee0
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2025-07-15 18:05:31 -0300

    fix: corrigindo arquivo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ ! -f /var/www/vendor/autoload.php ]; then
-    echo "vendor/autoload.php not found. Running composer install..."
-    composer install --no-interaction --prefer-dist --optimize-autoloader
-fi
-
-echo "Aguardando o PostgreSQL iniciar..."
-until pg_isready -h db -p 5432 -U user; do
-  sleep 1
-done
-
 echo "PostgreSQL está pronto. Executando as migrations..."
 php artisan migrate --force
 
